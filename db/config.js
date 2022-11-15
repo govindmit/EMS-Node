b21d9542903a33722c31d757da1252378d3f8785
ajay <ajaykushwah13k@gmail.com>
2022-11-15 12:04:51 +0530

    update

--- db/config.js
+++ db/config.js
@@ -1,3 +1,4 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/employee-management-system");
+mongoose.set('bufferCommands', false);
 console.log("database is connected");
